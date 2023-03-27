2fef11efba7b143a5666b8172202b522347d010e
gaffOverflow <gaffar.ibrahim18@gmail.com>
2023-03-26 19:00:48 -0700

    basic functionalities of changing player and textContent updating is added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const cells = document.querySelectorAll(".cell")
-const statusText = document.querySelector(".statusText")
-const restartBtn = document.querySelector(".restartBtn")
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector(".statusText");
+const restartBtn = document.querySelector(".restartBtn");
 const winConditions = [
   [0, 1, 2],
   [3, 4, 5],
@@ -9,5 +9,40 @@ const winConditions = [
   [1, 4, 7],
   [2, 5, 8],
   [0, 4, 8],
-  [2, 4, 6]
-]
+  [2, 4, 6],
+];
+
+let option = ["", "", "", "", "", "", "", "", ""];
+
+let currentPlayer = "x";
+let running = false;
+
+initializeGame()
+
+function initializeGame() {
+  cells.forEach(cell => cell.addEventListener("click", cellClicked));
+  restartBtn.addEventListener("click", restartGame);
+  statusText.textContent = `${currentPlayer}'s turns`;
+  running = true;
+}
+function cellClicked() {
+  const cellIndex = this.getAttribute("cellIndex");
+  if (option[cellIndex] != "" || !running) {
+    return;
+  }
+  updateCell(this, cellIndex);
+  changePlayer()
+  checkWinner();
+}
+function updateCell(cell, index) {
+  option[index] = currentPlayer;
+  cell.textContent = currentPlayer;
+}
+function changePlayer() {
+  currentPlayer = (currentPlayer == "x") ? "o" : "x"
+  statusText.textContent = `${currentPlayer}`
+}
+function checkWinner() {
+  
+}
+function restartGame() {}
